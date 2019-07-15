37e34f8d38af90f0d42ab3163ea7b829ef9cbcd7
syakisama <syakisama@yahoo.co.jp>
2019-07-15 18:02:35 +0800

    Modify Style: PostList > Card

--- header.php
+++ header.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
 <meta charset="<?php bloginfo( 'charset' ); ?>"/>
 <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 <meta http-equiv="Cache-Control" content="no-transform"/>
@@ -8,3 +11,31 @@
 <link rel="profile" href="http://gmpg.org/xfn/11"/>
 <title></title>
 <?php wp_head(); ?>
+</head>
+<body>
+<header id="kgm-header" style="height: 170px;">
+	<div class="kgm-header-bg" style="background-image:url(https://s2.ax1x.com/2019/06/13/VhEcNQ.png);"></div>
+	<div class="kgm-header-bg-blur" style="background-image:url(https://s2.ax1x.com/2019/06/13/VhEcNQ.png);"></div>
+	<nav class="kgm-primary-menu kgm-primary-menu-white">
+		<div class="grid-container">
+			<div class="row">
+				<h1 class="kgm-primary-menu-logo"><a href="<?php home_url(); ?>">网站标题</a></h1>
+				<?php
+					if(has_nav_menu('kgm-primary-menu-logout')){
+						$args = array(
+							'theme_location'  => 'kgm-primary-menu-logout',
+							'container_class' => 'kgm-fl kgm-nav-container',
+							'walker'          => new custom_navwalker
+						);
+						wp_nav_menu($args);
+						unset($args);
+					}
+				?>
+				<div class="kgm-fr">
+					<a class="kgm-primary-menu-user kgm-fl"><img src="https://s2.ax1x.com/2019/06/30/Zl0bHe.jpg"></a>
+					<a class="kgm-primary-menu-button kgm-fl">登录</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+</header>
--- index.php
+++ index.php
@@ -1,43 +1,32 @@
-<!DOCTYPE html>
-<html lang="zh-CN">
-<head>
 <?php get_header(); ?>
-</head>
-<body>
-<header id="kgm-header" style="height: 170px;">
-	<div class="kgm-header-bg" style="background-image:url(https://s2.ax1x.com/2019/06/13/VhEcNQ.png);"></div>
-	<div class="kgm-header-bg-blur" style="background-image:url(https://s2.ax1x.com/2019/06/13/VhEcNQ.png);"></div>
-	<nav class="kgm-primary-menu kgm-primary-menu-white">
-		<div class="grid-container">
-			<div class="row">
-				<h1 class="kgm-primary-menu-logo"><a href="<?php home_url(); ?>">网站标题</a></h1>
-				<?php
-					if(has_nav_menu('kgm-primary-menu-logout')){
-						$args = array(
-							'theme_location'  => 'kgm-primary-menu-logout',
-							'container_class' => 'kgm-fl kgm-nav-container',
-							'walker'          => new custom_navwalker
-						);
-						wp_nav_menu($args);
-						unset($args);
-					}
-				?>
-				<div class="kgm-fr">
-					<a class="kgm-primary-menu-user kgm-fl"><img src="https://s2.ax1x.com/2019/06/30/Zl0bHe.jpg"></a>
-					<a class="kgm-primary-menu-button kgm-fl">登录</a>
-				</div>
-			</div>
-		</div>
-	</nav>
-</header>
 <div id="kgm-app">
 	<div class="grid-container">
 		<div class="row">
-			<div class="col-12 col-md-8 kgm-main">1</div>
-			<div class="col-12 col-md-4 kgm-sidebar">2</div>
+			<div class="col-8 kgm-main">
+				<?php $posts = get_posts( "category=2&numberposts=6" ); ?>
+				<?php if( $posts ) : ?>
+					<div class="grid-container kgm-post-list">
+						<h2 class="kgm-post-list-title">分类标题</h2>
+						<div class="row">
+							<?php foreach( $posts as $post ) : setup_postdata( $post ); ?>
+								<div class="col-3 kgm-post-list-card">
+									<a class="kgm-post-list-card-link" href="#">
+										<div class="kgm-post-list-card-img" style="background-image:url(https://img.pic-imges.com/pic/upload/vod/2019-01/15464435286.jpg);">
+											<div class="kgm-post-list-card-meta">
+												<div class="kgm-post-list-card-meta-view"><i class="fa fa-heart-o" aria-hidden="true"></i>12341</div>
+												<div class="kgm-post-list-card-meta-like"><i class="fa fa-eye" aria-hidden="true"></i>2141</div>
+											</div>
+										</div>
+										<div class="kgm-post-list-card-title">12356</div>
+									</a>
+								</div>
+							<?php endforeach; ?>
+						</div>
+					</div>
+				<?php endif; ?>
+			</div>
+			<div class="col-4 kgm-sidebar">2</div>
 		</div>
 	</div>
 </div>
 <?php get_footer(); ?>
-</body>
-</html>
