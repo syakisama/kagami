f04a14bfda790bf4bd3df13f05c543cb025113de
syakisama <syakisama@yahoo.co.jp>
2019-07-31 00:08:09 +0800

    2019-07-30(2)
    Modify style: sidebar widgets

--- include/widgets/widget-init.php
+++ include/widgets/widget-init.php
@@ -3,7 +3,7 @@ function kgm_widgets_init(){
 	register_sidebar(array(
 		'name' => '首页侧边栏',
 		'id' => 'kgm-home-sidebar',
-		'before_widget' => '<aside id="kgm-%1$s" class="kgm-widget %2$s">',
+		'before_widget' => '<aside id="kgm-%1$s" class="kgm-widget kgm-%2$s">',
 		'after_widget' => '</aside>',
 		'before_title' => '<h2 class="kgm-widget-title">',
 		'after_title' => '</h2>'
@@ -16,6 +16,8 @@ function kgm_remove_default_widgets(){
 	unregister_widget('WP_Nav_Menu_Widget');
 	unregister_widget('WP_Widget_Calendar');
 	unregister_widget('WP_Widget_Search');
+	unregister_widget('WP_Widget_Recent_Posts');
+	unregister_widget('WP_Widget_Recent_Comments');
 	unregister_widget('WP_Widget_Media_Audio');
 	unregister_widget('WP_Widget_Media_Image');
 	unregister_widget('WP_Widget_Media_Video');
